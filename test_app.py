c423753107f74fc8537e9295806abec4233e209d
liperez78 <liliana_ibeth.perez@uao.edu.co>
2024-07-24 08:11:38 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,49 +1,22 @@
 import unittest
-from unittest.mock import patch, MagicMock
-import tkinter as tk
-from PIL import Image
-from app import App  # Asumiendo que el código original está en un archivo llamado app.py
+from detector_neumonia import App
 
-class TestApp(unittest.TestCase):
-    def setUp(self):
-        # Crear una instancia de la aplicación
-        self.app = App()
-        self.app.root.withdraw()  # Ocultar la ventana principal durante las pruebas
+class Testload_img_file(unittest.TestCase):
+    def test_1(self):
+        resultado = App.load_img_file('Torax.jpg')
+        self.assertTrue(resultado is not False, 'La imagen Torax.jpg no se cargó correctamente.')
 
-    @patch('tkinter.filedialog.askopenfilename')
-    @patch('PIL.Image.open')
-    def test_load_img_file(self, mock_open, mock_askopenfilename):
-        # Configurar los mocks
-        mock_askopenfilename.return_value = 'test_image.jpg'
-        mock_image = MagicMock(spec=Image.Image)
-        mock_open.return_value = mock_image
+    def test_2(self):
+        resultado = App.load_img_file('otra_imagen.jpg')
+        self.assertTrue(resultado is not False, 'La imagen otra_imagen.jpg no se cargó correctamente.')
 
-        # Llamar a la función de carga de imagen
-        self.app.load_img_file()
+    def test_3(self):
+        resultado = App.load_img_file(None)
+        self.assertFalse(resultado, 'El resultado debería ser False para una imagen nula.')
 
-        # Verificar que la imagen se ha cargado correctamente
-        mock_open.assert_called_once_with('test_image.jpg')
-        self.assertTrue(self.app.button1['state'] == 'normal')
-
-    @patch('tensorflow.keras.models.load_model')
-    @patch('numpy.expand_dims')
-    @patch('numpy.array')
-    def test_run_model(self, mock_array, mock_expand_dims, mock_load_model):
-        # Configurar el mock del modelo
-        mock_model = MagicMock()
-        mock_load_model.return_value = mock_model
-        mock_model.predict.return_value = [[0.7]]  # Supongamos que la predicción es 0.7
-
-        # Cargar una imagen falsa
-        self.app.image = Image.new('RGB', (150, 150))
-
-        # Llamar a la función de predicción
-        self.app.run_model()
-
-        # Verificar la predicción
-        mock_model.predict.assert_called_once()
-        self.assertEqual(self.app.text2.get("1.0", tk.END).strip(), "Neumonía")
-        self.assertEqual(self.app.text3.get("1.0", tk.END).strip(), "0.70")
+    def test_4(self):
+        resultado = App.load_img_file('imagen_inexistente.jpg')
+        self.assertFalse(resultado, 'El resultado debería ser False para una imagen inexistente.')
 
 if __name__ == '__main__':
     unittest.main()
